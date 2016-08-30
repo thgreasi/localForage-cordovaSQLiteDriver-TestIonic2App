30170ce487536bfd953bc15112afc4fb251e191b
Thodoris Greasidis <thgreasi@gmail.com>
2016-08-30 16:38:06 +0300

    feat(pages/home): add test driver button

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {simpleTest} from '../../localforageCordovasqlitedriverDemo';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -7,5 +8,9 @@ import {NavController} from 'ionic-angular';
 export class HomePage {
   constructor(private navController: NavController) {
   
+  }
+
+  testLocalForageDriver () {
+  	return simpleTest();
   }
 }
